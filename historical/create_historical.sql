ee0804eea0c65bec3ac0fba1ea9a57c6fde44c55
Brollemolle <brolinadam5@gmail.com>
2024-11-30 11:47:22 +0100

    Started formulating historical database.

--- historical/create_historical.sql
+++ historical/create_historical.sql
@@ -3,11 +3,32 @@ CREATE DATABASE historical_soundgood;
 
 \c historical_soundgood
 
-CREATE TABLE student_lesson (
-    lesson_type  VARCHAR(100)
+CREATE TYPE lesson_t AS ENUM ('Individual', 'Group', 'Ensemble');
+
+-- Create help table
+CREATE TABLE lookup_instrument (
+    instrument_type_id SERIAL PRIMARY KEY,
+    instrument_type VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE student_lesson_record (
+    id SERIAL PRIMARY KEY,
+    lesson_type lesson_t NOT NULL,
+    genre VARCHAR(100),
+    instrument_type_id int,
+    price float NOT NULL,
+    student_name VARCHAR(100),
+    student_email VARCHAR(100),
+
+    FOREIGN KEY (instrument_type_id) REFERENCES lookup_instrument (instrument_type_id)
 );
 
 
+INSERT INTO lookup_instrument (instrument_type) 
+VALUES  ('Piano'), ('Violin'), ('Flute'), 
+        ('Trumpet'), ('Drums'), ('Saxophone'), 
+        ('Guitar'), ('Cello'), ('Clarinet'), 
+        ('Accordion');
 
 
 
---
+++ historical/inject_historical.sql
@@ -0,0 +1,9 @@
+
+
+INSERT INTO student_lesson_record(lesson_type, genre, instrument_type_id, price, student_name, _student_email)
+SELECT
+    *
+FROM
+    
+
+
