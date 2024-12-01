f75335c0238134dc3b9611e1eddc874abdba7360
Brollemolle <brolinadam5@gmail.com>
2024-12-01 14:07:14 +0100

    Script for injecting data from DB to historical DB. Note that historical DB is just a separate table within DB.

--- historical/inject_historical.sql
+++ historical/inject_historical.sql
@@ -1,9 +1,48 @@
 
 
-INSERT INTO student_lesson_record(lesson_type, genre, instrument_type_id, price, student_name, _student_email)
+INSERT INTO student_lesson_record(lesson_type, genre, instrument_type_id, price, student_name, student_email)
 SELECT
-    *
+    'Individual' AS lesson_type,
+    NULL AS genre,
+    l.instrument_type_id,
+    p.cost,
+    s.name AS student_name,
+    s.email AS student_email
 FROM
-    
+    individual_lesson AS l
+INNER JOIN student_lesson AS c ON l.lesson_id = c.lesson_id
+INNER JOIN student AS s ON s.student_id = c.student_id
+INNER JOIN pricing AS p ON l.pricing_id = p.pricing_id;
+ 
 
+
+INSERT INTO student_lesson_record(lesson_type, genre, instrument_type_id, price, student_name, student_email)
+SELECT
+    'Group' AS lesson_type,
+    NULL AS genre,
+    l.instrument_type_id,
+    p.cost,
+    s.name AS student_name,
+    s.email AS student_email
+FROM
+    group_lesson AS l
+INNER JOIN student_lesson AS c ON l.lesson_id = c.lesson_id
+INNER JOIN student AS s ON s.student_id = c.student_id
+INNER JOIN pricing AS p ON l.pricing_id = p.pricing_id;
+ 
+
+
+INSERT INTO student_lesson_record(lesson_type, genre, instrument_type_id, price, student_name, student_email)
+SELECT
+    'Ensemble' AS lesson_type,
+    l.genre AS genre,
+    NULL AS instrument_type_id,
+    p.cost,
+    s.name AS student_name,
+    s.email AS student_email
+FROM
+    ensemble_lesson AS l
+INNER JOIN student_lesson AS c ON l.lesson_id = c.lesson_id
+INNER JOIN student AS s ON s.student_id = c.student_id
+INNER JOIN pricing AS p ON l.pricing_id = p.pricing_id;
 
